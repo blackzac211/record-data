62d40e2cc87dbdbdbcc9634d837181d8fd62d6f7
Administrator <Administrator@cucm-sync>
2020-11-17 11:14:17 +0900

    commit

--- src/main/java/unist/record/graph/controller/GraphController.java
+++ src/main/java/unist/record/graph/controller/GraphController.java
@@ -23,135 +23,141 @@ import unist.record.common.ExcelManager;
 
 @Controller
 public class GraphController {
-	
+
 	public static void main(String[] args) {
 		try {
 			ExcelManager excel = new ExcelManager();
-		    excel.setRow(0);
+			excel.setRow(0);
 			excel.setCell(0);
 			excel.setCellValue("Date");
 			excel.setCellValue("Max Count");
-			
-			DBManager db = new DBManager();
-    		String sql;
-    		String sql_detail;
-    		
-    		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
-    		// Calendar cal = Calendar.getInstance();
-    		// System.out.println("start: " + df.format(cal.getTime()));
-    		
-    		PreparedStatement pstmt;
-    		ResultSet rs;
-    		ResultSet rs_detail;
-    		
-    		Calendar startDate = Calendar.getInstance();
-    		startDate.set(2020, 2, 22, 0, 0, 0);
-    		Calendar endDate = Calendar.getInstance();
-    		endDate.set(2020, 5, 30, 23, 59, 59);
-    		String startTime;
-    		String endTime;
-    		String curTime;
-    		HashMap<String, Integer> map = new HashMap<String, Integer>();
-    		
-    		// for(int i = startDate; i <= endDate; i++) {
-    		String curDate;
-    		int detailCnt;
-    		int resCnt;
-    		int offset = 60;	// 검색 기준(1분)
-    		int i;
-    		while(true) {
-    			if(startDate.getTimeInMillis() > endDate.getTimeInMillis()) {
-    				break;
-    			}
-    			curDate = df.format(startDate.getTime());
-    			System.out.println("curDate: " + curDate);
-    			
-	    		sql = "SELECT metid, metdate, starttime, endtime, usercnt " + 
-	    				"FROM hero2 " + 
-	    				"WHERE metdate = ? " + 
-	    				"ORDER BY starttime ";
-	    		pstmt = db.getPreparedStatement(sql);
-	    		pstmt.setString(1, curDate);
-	    		rs = pstmt.executeQuery();
-	    		
-	    		map.put(curDate, 0);
-	    		while(rs.next()) {
-	    			startTime = rs.getString("starttime");
-	    			endTime = rs.getString("endtime");
-	    			
-	    			sql_detail = "SELECT SUM(usercnt) " + 
-	        				"FROM hero2 " + 
-	        				"WHERE metdate = ? AND ((starttime >= ? and starttime <= ?) OR (endtime >= ? AND endtime <= ?))"; 
-	    			
-	    			pstmt = db.getPreparedStatement(sql_detail);
-	    			pstmt.setString(1, curDate);
-	    			pstmt.setString(2, startTime);
-	        		pstmt.setString(3, endTime);
-	    			pstmt.setString(4, startTime);
-	        		pstmt.setString(5, endTime);
-	        		rs_detail = pstmt.executeQuery();
-	        		rs_detail.next();
-	        		detailCnt = rs_detail.getInt(1);
-	        		
-	        		i = 0;
-	        		if(map.get(curDate) < detailCnt) {
-	        			while(true) {
-		        			curTime = String.valueOf(Integer.valueOf(startTime) + (i++ * offset));
-		        			if(Integer.valueOf(endTime) < Integer.valueOf(curTime)) {
-		        				break;
-		        			}
-		        			
-		        			sql_detail = "SELECT SUM(usercnt)" + 
-		        					"FROM hero2 " + 
-		        					"WHERE metdate=? AND starttime <= ? AND endtime >= ?";
-		        			pstmt = db.getPreparedStatement(sql_detail);
-			    			pstmt.setString(1, curDate);
-			    			pstmt.setString(2, curTime);
-			        		pstmt.setString(3, curTime);
-			        		rs_detail = pstmt.executeQuery();
-			        		rs_detail.next();
-			        		resCnt = rs_detail.getInt(1);
-			        		
-			        		if(map.get(curDate) < resCnt) {
-			        			map.put(curDate, resCnt);
-			        		}
-	        			}
-	        		}
-	    		}
-	    		
-	    		excel.nextRow();
-		    	excel.setCell(0);
-		    	excel.setCellValue(curDate);
-		    	excel.setCellValue(String.valueOf(map.get(curDate)));
 
-    			startDate.add(Calendar.DATE, 1);
-    		}
-    		
+			DBManager db = new DBManager();
+			String sql;
+			String sql_detail;
+
+			SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+			// Calendar cal = Calendar.getInstance();
+			// System.out.println("start: " + df.format(cal.getTime()));
+
+			PreparedStatement pstmt;
+			ResultSet rs;
+			ResultSet rs_detail;
+
+			Calendar startDate = Calendar.getInstance();
+			startDate.set(2020, 6, 1, 0, 0, 0);
+			Calendar endDate = Calendar.getInstance();
+			endDate.set(2020, 7, 31, 23, 59, 59);
+			String startTime;
+			String endTime;
+			String curTime;
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+			// for(int i = startDate; i <= endDate; i++) {
+			String curDate;
+			int detailCnt;
+			int resCnt;
+			int offset = 60; // 검색 기준(1분)
+			int i;
+			while (true) {
+				if (startDate.getTimeInMillis() > endDate.getTimeInMillis()) {
+					break;
+				}
+				curDate = df.format(startDate.getTime());
+				System.out.println("curDate: " + curDate);
+				
+				// sql = "SELECT metid, metdate, starttime, endtime, usercnt " + "FROM hero2 " + "WHERE metdate = ? "+ "ORDER BY starttime ";
+				
+				sql = "SELECT rec_id, rec_startdate, rec_starttime, rec_enddate, rec_endtime " +
+						"FROM rec_data " +
+						"WHERE rec_startdate = ? " + 
+						"ORDER BY rec_starttime ";
+				
+				pstmt = db.getPreparedStatement(sql);
+				pstmt.setString(1, curDate);
+				rs = pstmt.executeQuery();
+
+				map.put(curDate, 0);
+				while (rs.next()) {
+					startTime = rs.getString("rec_starttime");
+					endTime = rs.getString("rec_endtime");
+
+					// sql_detail = "SELECT SUM(usercnt) FROM rec_data " + "WHERE metdate = ? AND ((starttime >= ? and starttime <= ?) OR (endtime >= ? AND endtime <= ?))";
+					sql_detail = "SELECT count(*) " + 
+							"FROM rec_data " + 
+							"WHERE rec_startdate = ? AND ((rec_starttime >= ? and rec_starttime <= ?) " +
+								"OR (rec_endtime >= ? AND rec_endtime <= ?))";
+					
+					pstmt = db.getPreparedStatement(sql_detail);
+					pstmt.setString(1, curDate);
+					pstmt.setString(2, startTime);
+					pstmt.setString(3, endTime);
+					pstmt.setString(4, startTime);
+					pstmt.setString(5, endTime);
+					rs_detail = pstmt.executeQuery();
+					rs_detail.next();
+					detailCnt = rs_detail.getInt(1);
+
+					i = 0;
+					if (map.get(curDate) < detailCnt) {
+						while (true) {
+							curTime = String.valueOf(Integer.valueOf(startTime) + (i++ * offset));
+							if (Integer.valueOf(endTime) < Integer.valueOf(curTime)) {
+								break;
+							}
+
+							// sql_detail = "SELECT SUM(usercnt)" + "FROM hero2 " + "WHERE metdate=? AND starttime <= ? AND endtime >= ?";
+							sql_detail = "SELECT count(*) " + 
+									"FROM rec_data " + 
+									"WHERE rec_startdate=? AND rec_starttime <= ? AND rec_endtime >= ?";
+							
+							pstmt = db.getPreparedStatement(sql_detail);
+							pstmt.setString(1, curDate);
+							pstmt.setString(2, curTime);
+							pstmt.setString(3, curTime);
+							rs_detail = pstmt.executeQuery();
+							rs_detail.next();
+							resCnt = rs_detail.getInt(1);
+
+							if (map.get(curDate) < resCnt) {
+								map.put(curDate, resCnt);
+							}
+						}
+					}
+				}
+
+				excel.nextRow();
+				excel.setCell(0);
+				excel.setCellValue(curDate);
+				excel.setCellValue(String.valueOf(map.get(curDate)));
+
+				startDate.add(Calendar.DATE, 1);
+			}
+
 			File file = new File("E:/export_record.xls");
-		    OutputStream out = new FileOutputStream(file);
-		    excel.getWorkbook().write(out);
-		    out.close();
-		} catch(Exception e) {
+			OutputStream out = new FileOutputStream(file);
+			excel.getWorkbook().write(out);
+			out.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	  @RequestMapping("/graph/selectRecordLogs.do")
-	    public void selectRecordLogs(String id_up, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-	    	try {
-	    		if(!CommonSecurity.checkReferer(request)) {
-	    			throw new Exception("Exploiting cross-site scripting in Referer header.");
-	    		}
-	    		
-	    		
-	    		JSONObject json = new JSONObject();
-	        	// json.put("list", list);
-	        	response.setContentType("text/json;charset=utf-8");
-	        	response.getWriter().print(json.toString());
-	    	} catch(Exception e) {
-	    		e.printStackTrace();
-	    	}
-	    }
-	  
-}
 
+	@RequestMapping("/graph/selectRecordLogs.do")
+	public void selectRecordLogs(String id_up, HttpServletRequest request, HttpServletResponse response,
+			HttpSession session) {
+		try {
+			if (!CommonSecurity.checkReferer(request)) {
+				throw new Exception("Exploiting cross-site scripting in Referer header.");
+			}
+
+			JSONObject json = new JSONObject();
+			// json.put("list", list);
+			response.setContentType("text/json;charset=utf-8");
+			response.getWriter().print(json.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
